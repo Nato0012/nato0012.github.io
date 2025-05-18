a2997bf35af97257d6aafbb8a8bfd56348cb9e56
Nato0012 <melodynathana@gmail.com>
2025-05-19 11:26:08 +1200

    Update getEnv.js

--- api/getEnv.js
+++ api/getEnv.js
@@ -1,4 +1,9 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await new Promise(resolve => setTimeout(resolve, 50));
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+  
   res.json({
     jsonBinKey: process.env.JSONBIN_KEY,
     donorId: process.env.DONOR_ID,
